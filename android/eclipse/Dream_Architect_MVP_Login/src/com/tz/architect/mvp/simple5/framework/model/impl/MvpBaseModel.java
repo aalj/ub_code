0d99671f299e898fbae51afceff8e98c7e78f8cc
aalj <dqliangjun@126.com>
2017-01-14 12:21:20 +0800

    备份性提交

--- android/eclipse/Dream_Architect_MVP_Login/src/com/tz/architect/mvp/simple5/framework/model/impl/MvpBaseModel.java
+++
@@ -1,7 +0,0 @@
-package com.tz.architect.mvp.simple5.framework.model.impl;
-
-import com.tz.architect.mvp.simple5.framework.model.MvpModel;
-
-public abstract class MvpBaseModel implements MvpModel{
-
-}
--- android/eclipse/Dream_Architect_MvpFramework2/src/com/tz/mvp/framework/base/model/impl/MvpBaseModel.java
+++
@@ -1,7 +0,0 @@
-package com.tz.mvp.framework.base.model.impl;
-
-import com.tz.mvp.framework.base.model.MvpModel;
-
-public abstract class MvpBaseModel implements MvpModel{
-
-}
