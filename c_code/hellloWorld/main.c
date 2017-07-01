d2ed3768aa5ab57a0b22dbcffcc6ce94a9e49929
aalj <dqliangjun@126.com>
2017-07-01 22:58:09 +0800

    C语言第四天学习

--- c_code/hellloWorld/main.c
+++ c_code/hellloWorld/main.c
@@ -1,69 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include<float.h>
+int mRandom(int x,int y);
 
-int main(int aa, int arg[])
+int main1(int aa, int arg[])
 {
-   /* jisuan();
-    int param = 0;
-    int param2= 0;
-    float param3 = 0;
-    printf("输入第一个数\n");
-    scanf("%d",&param);
-
-    printf("输入第一个数  %d\n",param);
-    printf("输入第二数\n");
-    scanf("%d",&param2);
-
-    printf("输入第二个数  %d\n",param2);
-    printf("输入第三数\n");
-    scanf("%f",&param3);
-
-    printf("输入第三个数  %f\n",param3);
-
-    printf("%d / %d = %d\n",param,param2,param/param2);
-    printf("%d / %d = %f\n",param,param2,param/param2);
-    printf("%d / %f = %f\n",param,param3,param/param3);*/
-
-int age =2;
-  switch (age)
-{
-  case 2:
-    printf("宝宝，你好 !");
-    //跳出该次的执行， 如果不写则会继续判断下一行的条件是否满足 知道遇到 break 语句  该switch执行结束
-    //break;
-  case 6:
-    printf("小朋友，你好 !");
-   // break;
-  case 12:
-    printf("少年，你好 !");
-   // break;
-  case 16:
-    printf("青少年，你好 !");
-   // break;
-  case 18:
-    printf("成年人，你好 !");
-   // break;
-  case 67:
-    printf("爷爷，你好 !");
-   // break;
-  default:// 以上条件都没有符合的执行这个条语句。   在没有break 语句的时候这句也会执行；
-    printf("对你的年龄我还没有对应的问候方式 ");
-    break;
-}
-
+    int index = 0;
+    int max=10;
+    int min=0;
+    for(index = 0; index<100;index++)
+    {
+        printf("第%d次随机数是%d\n",index,mRandom(max,min));
+    }
+    printf("return");
     return 0;
 }
 
-/*void jisuan(){
-
-    int num=0;
-    for(int i =0;i<10;i++)
+int main()
+{
+   //srand(time(NULL));
+   srand(time(NULL));
+    for (int i=0; i<10; i++)
     {
-        ++num;
+        printf("%d ", rand()%10);
+    }
+
+    for (int i=0; i<10; i++)
+    {
+        printf("%d \n", mRandom(100,1));
     }
 
 
-    printf("%d",num);
+  /* 程序的循环部分， 如果用户没猜中数字，就一直进行循环 */
 }
-*/
+
+
+
+int  mRandom(int max,int min)
+    {
+        //srand(time(NULL));
+        printf("%d\n",time(NULL));
+      return  (rand()%(max-min+1)+min);
+      //return  (rand()%(max-min+1)+min);
+    }
--- c_code/random/random.c
+++ c_code/random/random.c
@@ -1,31 +1,31 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
-//int mRandom(int x, int y );
+int mRandom(int x, int y );
 
 
 /**
 
 这是一个关于随机数的程序
 */
-void main3(int a , int arg[])
+void main(int a , int arg[])
     {
+    
+        srand(time(NULL));
         //random();
-        int index =  0 ;
+        int index =  0 ; 
         int max=10;
-        int min=1;
+        int min=0;
         for(index = 0; index<100;index++)
         {
-            printf("第  次随机数是 %d \n",index,mRandom2(max,min));
+            printf("第%d次随机数是%d\n",index,mRandom(max,min));
         }
         printf("hello world\n");
-        return 0;
     }
 
 
-int  mRandom2(int max,int min)
+int  mRandom(int max,int min)
     {
-        srand(time(NULL));
         //printf("%l",time(NULL));
       return  (rand()%(max-min+1)+min);
     }
