4be75361f3d53840ee4364f145e29dc043d80330
aalj <dqliangjun@126.com>
2017-08-25 10:35:32 +0800

    RecyclerView 加载不同布局的适配器，  把所有的数据处理和其他的内容  全部由activity 处理

---
+++ android/idea/SuZhouBus/app/src/main/java/net/lll0/bus/adapter/test/RecyclerViewNotHeadFootAdapter.java
@@ -0,0 +1,126 @@
+package net.lll0.bus.adapter.test;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import net.lll0.bus.adapter.BaseRecyclerAdapter;
+import net.lll0.bus.suzhoubus.R;
+import net.lll0.bus.adapter.RecyclerViewHolder;
+
+import java.util.List;
+
+/**
+ * Created by liang on 2017/8/24.
+ */
+
+public abstract class RecyclerViewNotHeadFootAdapter<T extends Bean> extends RecyclerView.Adapter<RecyclerViewHolder> {
+    /**
+     * 加载不同的两种方式
+     * 1. 传入不同的数据源，对应的位置加载不同的布局
+     * 2. 传入一个数据源，但是通过数据源里面特殊的字段判断加载什么布局
+     */
+    private List<T> bean;
+    private Context mContext;
+
+    private OnItemClickListener mClickListener;
+    private OnItemLongClickListener mLongClickListener;
+
+    public RecyclerViewNotHeadFootAdapter(List<T> bean, Context mContext) {
+        this.bean = bean;
+        this.mContext = mContext;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        //通过这个区分加载不同 view
+        //通过判断特殊的字段加载不同的布局
+
+        return bean.get(position).getType();
+    }
+
+    @Override
+    public RecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        //通过 getTtemViewType 返回的 内容加载不同 的布局
+
+        //假设加载 三中布局分别对应 1 2 3
+        final RecyclerViewHolder recyclerViewHolder = myCreateViewHolder(parent, viewType);
+
+
+        if (mClickListener != null) {
+
+
+            recyclerViewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mClickListener.onItemClick(recyclerViewHolder.itemView, recyclerViewHolder.getLayoutPosition());
+                }
+            });
+        }
+        if (mLongClickListener != null) {
+            recyclerViewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mLongClickListener.onItemLongClick(recyclerViewHolder.itemView,recyclerViewHolder.getLayoutPosition());
+                }
+            });
+        }
+
+
+        return recyclerViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerViewHolder holder, int position) {
+        //onCreateViewHolder 为不同的布局绑定对应的数据
+        myBindViewHolder(holder, position);
+    }
+
+    @Override
+    public int getItemCount() {
+        //主要是计算 加载数据的总数
+        return bean.size();
+    }
+
+
+    /**
+     * 调用item的点击事件就是调用这个方法，然后实现监听事件的接口
+     *
+     * @param listener
+     */
+    final public void setOnItemClickListener(OnItemClickListener listener) {
+        mClickListener = listener;
+    }
+
+    /**
+     * 调用item长的点击事件就是调用这个方法，然后实现监听事件的接口
+     *
+     * @param listener
+     */
+    final public void setOnItemLongClickListener(OnItemLongClickListener listener) {
+        mLongClickListener = listener;
+    }
+
+    abstract protected RecyclerViewHolder myCreateViewHolder(ViewGroup parent, int viewType);
+
+    abstract protected void myBindViewHolder(RecyclerViewHolder holder, int position);
+
+
+    /**
+     * 实现每一个item的点击事件需要实现的方法
+     */
+    public interface OnItemClickListener {
+        void onItemClick(View itemView, int pos);
+    }
+
+    /**
+     * 实现item的长顶啊及事件需要实现的方法
+     */
+    public interface OnItemLongClickListener {
+        void onItemLongClick(View itemView, int pos);
+    }
+
+}
--- android/idea/SuZhouBus/app/src/main/java/net/lll0/bus/ui/test_activity/RecyclerViewTestActivity.java
+++ android/idea/SuZhouBus/app/src/main/java/net/lll0/bus/ui/test_activity/RecyclerViewTestActivity.java
@@ -4,40 +4,45 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
+import net.lll0.bus.adapter.RecyclerViewHolder;
 import net.lll0.bus.adapter.test.Bean;
-import net.lll0.bus.adapter.test.RecyclerViewAdapter;
+import net.lll0.bus.adapter.test.RecyclerViewNotHeadFootAdapter;
 import net.lll0.bus.suzhoubus.R;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class RecyclerViewTestActivity extends Activity {
+public class RecyclerViewTestActivity extends Activity implements RecyclerViewNotHeadFootAdapter.OnItemClickListener {
 
     private RecyclerView mRecycler;
     private Activity mActitivity;
-    private RecyclerViewAdapter recyclerViewAdapter;
+    private RecyclerViewNotHeadFootAdapter recyclerViewNotHeadFootAdapter;
     private List<Bean> beans;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler_view_test);
-        mActitivity = this ;
-       initData();
+        mActitivity = this;
+        initData();
         initView();
     }
 
     private void initData() {
         beans = new ArrayList<>();
-        Bean bean ;
+        Bean bean;
         for (int i = 0; i < 10; i++) {
             bean = new Bean();
-            if (i==0) {
+            if (i == 0) {
                 bean.setType(1);
                 beans.add(bean);
                 continue;
-            }else if(i==9){
+            } else if (i == 9) {
                 bean.setType(3);
                 beans.add(bean);
                 continue;
@@ -50,7 +55,40 @@ public class RecyclerViewTestActivity extends Activity {
     private void initView() {
         mRecycler = (RecyclerView) findViewById(R.id.recycler);
         mRecycler.setLayoutManager(new LinearLayoutManager(mActitivity));
-        recyclerViewAdapter = new RecyclerViewAdapter(beans,mActitivity);
-        mRecycler.setAdapter(recyclerViewAdapter);
+        recyclerViewNotHeadFootAdapter = new RecyclerViewNotHeadFootAdapter<Bean>(beans, mActitivity) {
+            @Override
+            protected RecyclerViewHolder myCreateViewHolder(ViewGroup parent, int viewType) {
+                RecyclerViewHolder recyclerViewHolder = null;
+                if (1 == viewType) {
+                    recyclerViewHolder = new RecyclerViewHolder(mActitivity, LayoutInflater.from(mActitivity).inflate(R.layout.item, parent, false), viewType);
+                } else if (2 == viewType) {
+                    recyclerViewHolder = new RecyclerViewHolder(mActitivity, LayoutInflater.from(mActitivity).inflate(R.layout.item_lineinfo, parent, false), viewType);
+                } else if (3 == viewType) {
+                    recyclerViewHolder = new RecyclerViewHolder(mActitivity, LayoutInflater.from(mActitivity).inflate(R.layout.nav_header_home, parent, false), viewType);
+                }
+                return recyclerViewHolder;
+            }
+
+            @Override
+            protected void myBindViewHolder(RecyclerViewHolder holder, int position) {
+                Bean bean = beans.get(position);
+                int type = bean.getType();
+                if (1 == type) {
+                    holder.setText(R.id.textView2, "textView2");
+                    holder.setText(R.id.textView3, "textView3");
+                } else if (2 == type) {
+                    holder.setText(R.id.lineinfo_index, position + "");
+                } else if (3 == type) {
+
+                }
+            }
+        };
+        mRecycler.setAdapter(recyclerViewNotHeadFootAdapter);
+        recyclerViewNotHeadFootAdapter.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(View itemView, int pos) {
+        Toast.makeText(mActitivity,pos+"",Toast.LENGTH_SHORT).show();
     }
 }
